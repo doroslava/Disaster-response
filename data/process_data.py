e0b697fcf97b021a2b1710a1090b656f399d3dd2
root <dora.sribar@gmail.com>
2020-05-29 19:07:56 +0000

    feature: Add process data pipeline and train classifier pipeline

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,87 @@
+import os
 import sys
-
+import pandas as pd
+from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    pass
-
+    '''
+    Loads and merges data frames with messages and information about categories.
+    
+        Parameters:
+            messages_filepath (string): csv file with information about disaster messages
+            categories_filepath (string): csv file with information about disaster categories
+            
+        Returns:
+            df (data frame): merged pandas dataframe with disaster messages and categories for the messages 
+    
+    '''
+    
+    messages = pd.read_csv(messages_filepath) 
+    categories = pd.read_csv(categories_filepath)
+    
+    df = pd.merge(messages, categories, on = "id")
+    
+    return df
 
 def clean_data(df):
-    pass
-
-
+    '''
+    Preprocesses the disaster response data for the machine-learning pipeline. 
+        
+        Parameters:
+            df (data frame): input pandas data frame to be cleaned
+        Returns:
+            df_final(data frame): cleaned pandas data frame
+            
+    
+    '''
+    
+    
+    # Create a dataframe of the individual category columns.
+    categories = df["categories"].str.split(pat=";", expand=True)
+    
+    # Select the first row of the categories column and use this row to extract a list of new column names for categories.
+    row = categories.iloc[1,:]
+    category_colnames = row.str.split("-").str.get(0)
+    
+    # Rename the newly extracted columns for the former categories column
+    categories.columns = category_colnames
+    
+    # Convert category values to just numbers 0 or 1. Iterate through the category columns in df to keep only the last character of 
+    # each string (the 1 or 0). For example, related-0 becomes 0, related-1 becomes 1. Convert the string to a numeric value.
+    
+    for column in categories:
+    # Set each value to be the last character of the string.
+        categories[column] = categories[column].str.split("-").str.get(1)    
+    # Convert column from string to numeric.
+        categories[column] = pd.to_numeric(categories[column])
+    
+    # Replace categories column in df with newly extracted category columns.
+    df.drop(columns="categories", inplace=True)
+    df_final = pd.concat([df, categories], axis = 1)
+    
+    # Some of the columns have category 2, which is incorrect. Replace them with 0s.     
+    df_final.replace(2,0,inplace=True)
+    
+    # Remove duplicates.
+    df_final.drop_duplicates(inplace=True)
+    
+    return df_final
+    
+    
 def save_data(df, database_filename):
-    pass  
+    '''
+    Saves cleaned data to the sqlite database.
+    
+        Parameters: 
+            df (data frame): pandas data frame with cleaned data
+            database_filename (string): string with relative path and name of the sqlite database in which to save cleaned data
+        Returns:
+            None
+    '''
+    database_file_path = "sqlite:///" + database_filename
+    engine = create_engine(database_file_path)
+    
+    df.to_sql('messages_cleaned', engine, index=False)
 
 
 def main():
--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,128 @@
 import sys
+from sqlalchemy import create_engine
+import pandas as pd
+import re
 
+import nltk
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('stopwords')
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import GridSearchCV
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.svm import LinearSVC
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+
+import pickle
 
 def load_data(database_filepath):
-    pass
-
-
+    '''
+    Reads cleaned data frame from sqlite database. 
+        Parameteres:
+            database_filepath (string): sqlite database from which to read the cleaned data
+        Returns:
+            X (data frame): pandas dataframe with predictor variable message
+            Y (data frame): pandas dataframe with response variables, disaster response categories
+            category_names(list): list of category names for the response variables
+            
+    '''
+    # Load data from database.
+    engine = create_engine('sqlite:///' + database_filepath)
+    df = pd.read_sql_table("messages_cleaned", engine)
+    
+    # Assign predictor and response variables
+    X = df.iloc[:,1]
+    Y = df.iloc[:,4:]
+    
+    # Drop child alone column since it does not have values for the 1 category
+    Y.drop("child_alone", inplace=True, axis = 1)
+    
+    # Assign category names
+    category_names = Y.columns
+    
+    return (X, Y, category_names)
+    
 def tokenize(text):
-    pass
+    '''
+    Tokenize text data.
+        Parameters:
+            text (string): Text to tokenize
+        Returns:
+            clean_text(list): List of words from tokenized text
+    
+    '''
+    # Remove non word/number characters.
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text) 
+    
+    # Tokenize text
+    tokens = word_tokenize(text)
+    
+    # Initialize lemmatizer
+    lemmatizer = WordNetLemmatizer()
+    
+    # Lematize words, convert to lower case, and remove stop words 
+    clean_tokens = []
+    
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        if clean_tok not in stopwords.words("english"):
+            clean_tokens.append(clean_tok)
+    
+    return clean_tokens
 
 
 def build_model():
-    pass
-
+    '''
+    Builds machine learning pipeline for natural language processing. First words corpus is 
+    analyzed with Tfidf transformer and this is fed into random forest classificator.
+    Hypeparameter tuning is performed with grid search.
+        
+        Parameters:
+            None
+        Returns:
+            scikit model
+    '''
+    # Text processing and model pipeline.
+    pipeline = Pipeline([
+    ('vect', CountVectorizer(tokenizer=tokenize, max_df=0.4)), 
+    ('tfidf', TfidfTransformer()),
+    ('cmo', MultiOutputClassifier(LinearSVC(random_state=1, class_weight='balanced')))
+     ])
+    
+    # Define parameters for GridSearchCV.
+    parameters = {
+        'cmo__estimator__C': (0.1, 1, 10)
+    }
+    # Create gridsearch object and return as final model pipeline.
+   
+    model = GridSearchCV(pipeline, param_grid=parameters)
+    
+    return model
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    Y_pred = model.predict(X_test)
+    for i, category in enumerate (category_names):
+        print(classification_report(Y_test[category], y_pred.T[i], labels = [0, 1]))
 
 
 def save_model(model, model_filepath):
-    pass
+    '''
+    Export model as a pickle file.
+        Parameters:
+            model: model to export
+            model_filepath (string): pickle file for saving            
+        Returns:
+            None
+    '''
+    
+    # Export model as a pickle file.
+    pickle.dump(model, open(model_filepath, 'wb'))
 
 
 def main():
