161a39e8c15ab45de721d95641abf8cabb7665c1
Dora Sribar <dora.sribar@gmail.com>
2020-05-31 15:42:58 +0200

    feature: Add prequisites to run on Udacity Workspace

--- disaster_app.py
+++ disaster_app.py
@@ -1,6 +1,7 @@
 from disaster_app import app
 
 #Run locally on computer
-#app.run(host='localhost', port=5000, debug=True)
+app.run(host='localhost', port=5000, debug=True)
 
 #Run on the Udacity workspace
+#app.run(host='0.0.0.0', port=3001, debug=True)
